ba5085e16a41f062dd729accafd21a357d9f451e
Siju Joseph <siju-joseph@netspective.in>
2025-12-05 10:47:00 +0530

    Bump Spry formulas to 0.90.0

--- Formula/spry-runbook.rb
+++ Formula/spry-runbook.rb
@@ -1,21 +1,21 @@
 class SpryRunbook < Formula
   desc "Spry Runbook CLI - A runbook execution tool"
   homepage "https://github.com/programmablemd/packages"
-  version "0.80.0"
+  version "0.90.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/programmablemd/packages/releases/download/v0.80.0/spry-runbook-macos.tar.gz"
-      sha256 "09c21b73030e8e343654a3cb4e3733d54b6e0ac01ad17ac18c9eaaf43be262d3"
+      url "https://github.com/programmablemd/packages/releases/download/v0.90.0/spry-runbook-macos.tar.gz"
+      sha256 "b6d9ff5e2c48a5c37d050734b6453db573ad31016a8bd34a75c8813c64f514f0"
     else
-      url "https://github.com/programmablemd/packages/releases/download/v0.80.0/spry-runbook-macos.tar.gz"
+      url "https://github.com/programmablemd/packages/releases/download/v0.90.0/spry-runbook-macos.tar.gz"
       sha256 "09c21b73030e8e343654a3cb4e3733d54b6e0ac01ad17ac18c9eaaf43be262d3"
     end
   end
 
   on_linux do
-    url "https://github.com/programmablemd/packages/releases/download/v0.80.0/spry-sqlpage_0.80.0-ubuntu22.04u1_amd64.deb"
+    url "https://github.com/programmablemd/packages/releases/download/v0.90.0/spry-runbook_0.90.0-ubuntu22.04u1_amd64.deb"
     sha256 "427e449fb6c24c1b0eb48ddd83c3c65c1f8002674dca80886d4df099360808ba"
   end
 
@@ -24,7 +24,7 @@ class SpryRunbook < Formula
       bin.install "spry-runbook-macos" => "spry-runbook"
     elsif OS.linux?
       # For Linux, extract the DEB package using dpkg-deb
-      system "dpkg-deb", "-x", "spry-sqlpage_0.80.0-ubuntu22.04u1_amd64.deb", "."
+      system "dpkg-deb", "-x", "spry-runbook_0.90.0-ubuntu22.04u1_amd64.deb", "."
       bin.install "usr/bin/spry-runbook"
     end
   end
--- Formula/spry-sqlpage.rb
+++ Formula/spry-sqlpage.rb
@@ -1,21 +1,21 @@
 class SprySqlpage < Formula
   desc "Spry SQLPage CLI - A declarative web application framework"
   homepage "https://github.com/programmablemd/packages"
-  version "0.80.0"
+  version "0.90.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/programmablemd/packages/releases/download/v0.80.0/spry-sqlpage-macos.tar.gz"
-      sha256 "c37e5835d8b3e1dc58638eb69b8052d4a7336b3a1820e0b124f8fcb3dfd5211e"
+      url "https://github.com/programmablemd/packages/releases/download/v0.90.0/spry-sqlpage-macos.tar.gz"
+      sha256 "452e879cb8b2040b09d9bf40a47b49ceeb9d9dcf14ddbe15d9298c960eb452fc"
     else
-      url "https://github.com/programmablemd/packages/releases/download/v0.80.0/spry-sqlpage-macos.tar.gz"
+      url "https://github.com/programmablemd/packages/releases/download/v0.90.0/spry-sqlpage-macos.tar.gz"
       sha256 "c37e5835d8b3e1dc58638eb69b8052d4a7336b3a1820e0b124f8fcb3dfd5211e"
     end
   end
 
   on_linux do
-    url "https://github.com/programmablemd/packages/releases/download/v0.80.0/spry-sqlpage_0.80.0-ubuntu22.04u1_amd64.deb"
+    url "https://github.com/programmablemd/packages/releases/download/v0.90.0/spry-sqlpage_0.90.0-ubuntu22.04u1_amd64.deb"
     sha256 "427e449fb6c24c1b0eb48ddd83c3c65c1f8002674dca80886d4df099360808ba"
   end
 
@@ -24,7 +24,7 @@ class SprySqlpage < Formula
       bin.install "spry-sqlpage-macos" => "spry-sqlpage"
     elsif OS.linux?
       # For Linux, extract the DEB package using dpkg-deb
-      system "dpkg-deb", "-x", "spry-sqlpage_0.80.0-ubuntu22.04u1_amd64.deb", "."
+      system "dpkg-deb", "-x", "spry-sqlpage_0.90.0-ubuntu22.04u1_amd64.deb", "."
       bin.install "usr/bin/spry-sqlpage"
     end
   end
