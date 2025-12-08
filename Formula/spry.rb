b6d1b2d2debff19c28792d1f7b24b5b5a123d2c4
Siju Joseph <siju-joseph@netspective.in>
2025-12-08 11:48:36 +0530

    Bump Spry formula to 0.100.7

--- Formula/spry.rb
+++ Formula/spry.rb
@@ -1,13 +1,13 @@
 class Spry < Formula
   desc "Spry CLI - A declarative web application framework"
   homepage "https://github.com/programmablemd/packages"
-  version "vv0.100.7"
+  version "0.100.7"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/programmablemd/packages/releases/download/vv0.100.7/spry-macos.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "39c5f443cd5d674e3daf7d1b46c859debf00ad6201415a0275a3c53a6a8835f8"
     else
       url "https://github.com/programmablemd/packages/releases/download/vv0.100.7/spry-macos.tar.gz"
       sha256 "452e879cb8b2040b09d9bf40a47b49ceeb9d9dcf14ddbe15d9298c960eb452fc"
@@ -15,7 +15,7 @@ class Spry < Formula
   end
 
   on_linux do
-    url "https://github.com/programmablemd/packages/releases/download/vv0.100.7/spry-sqlpage_v0.100.7-ubuntu22.04u1_amd64.deb"
+    url "https://github.com/programmablemd/packages/releases/download/vv0.100.7/spry-sqlpage_0.100.7-ubuntu22.04u1_amd64.deb"
     sha256 "8fc97288dbc5259e30b48fe25438993698f8e834fd9904f7f191248d91e23de1"
   end
 
@@ -24,7 +24,7 @@ class Spry < Formula
       bin.install "spry-macos" => "spry"
     elsif OS.linux?
       # For Linux, extract the DEB package using dpkg-deb
-      system "dpkg-deb", "-x", "spry-sqlpage_v0.100.7-ubuntu22.04u1_amd64.deb", "."
+      system "dpkg-deb", "-x", "spry-sqlpage_0.100.7-ubuntu22.04u1_amd64.deb", "."
       bin.install "usr/bin/spry"
     end
   end
