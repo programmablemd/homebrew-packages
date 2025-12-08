a4f3ad4a8a8b80fa5b38f248667ad5b6f4f5d1eb
Siju Joseph <siju-joseph@netspective.in>
2025-12-08 11:46:51 +0530

    Bump Spry formula to v0.100.7

--- Formula/spry.rb
+++ Formula/spry.rb
@@ -1,21 +1,21 @@
 class Spry < Formula
   desc "Spry CLI - A declarative web application framework"
   homepage "https://github.com/programmablemd/packages"
-  version "0.90.0"
+  version "vv0.100.7"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/programmablemd/packages/releases/download/v0.90.0/spry-macos.tar.gz"
-      sha256 "452e879cb8b2040b09d9bf40a47b49ceeb9d9dcf14ddbe15d9298c960eb452fc"
+      url "https://github.com/programmablemd/packages/releases/download/vv0.100.7/spry-macos.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
-      url "https://github.com/programmablemd/packages/releases/download/v0.90.0/spry-macos.tar.gz"
+      url "https://github.com/programmablemd/packages/releases/download/vv0.100.7/spry-macos.tar.gz"
       sha256 "452e879cb8b2040b09d9bf40a47b49ceeb9d9dcf14ddbe15d9298c960eb452fc"
     end
   end
 
   on_linux do
-    url "https://github.com/programmablemd/packages/releases/download/v0.90.0/spry-sqlpage_0.90.0-ubuntu22.04u1_amd64.deb"
+    url "https://github.com/programmablemd/packages/releases/download/vv0.100.7/spry-sqlpage_v0.100.7-ubuntu22.04u1_amd64.deb"
     sha256 "8fc97288dbc5259e30b48fe25438993698f8e834fd9904f7f191248d91e23de1"
   end
 
@@ -24,7 +24,7 @@ class Spry < Formula
       bin.install "spry-macos" => "spry"
     elsif OS.linux?
       # For Linux, extract the DEB package using dpkg-deb
-      system "dpkg-deb", "-x", "spry-sqlpage_0.90.0-ubuntu22.04u1_amd64.deb", "."
+      system "dpkg-deb", "-x", "spry-sqlpage_v0.100.7-ubuntu22.04u1_amd64.deb", "."
       bin.install "usr/bin/spry"
     end
   end
