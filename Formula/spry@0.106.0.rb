11579195acb0d85aaf11990c2375c377c396de3f
Sreevidya Aravind <sreevidya-aravind@netspective.in>
2025-12-15 10:15:02 +0530

    Bump Spry formula to 0.106.0

--- Formula/spry@0.106.0.rb
+++ Formula/spry@0.106.0.rb
@@ -1,4 +1,4 @@
-class Spry < Formula
+class SpryAT01060 < Formula
   desc "Spry CLI - A declarative web application framework"
   homepage "https://github.com/programmablemd/packages"
   version "0.106.0"
@@ -33,4 +33,3 @@ class Spry < Formula
     system "#{bin}/spry", "--version"
   end
 end
-
