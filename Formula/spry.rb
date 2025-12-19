8b22fe2fe3fccc649a1ca2466692b14787fc6211
Siju Joseph <siju-joseph@netspective.in>
2025-12-19 13:09:16 +0530

    Bump Spry formula to 0.109.2

--- Formula/spry.rb
+++ Formula/spry.rb
@@ -7,16 +7,16 @@ class Spry < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/programmablemd/packages/releases/download/v0.109.2/spry-macos.tar.gz"
-      sha256 "963ac1e88047484abfafcc8aadc8a283c648c3fc1cb7cea010031ba17010d86b"
+      sha256 "2d46913afd9bf06113ca530d5368b710519e45d22bdf931be87aa07a778cb9cc"
     else
       url "https://github.com/programmablemd/packages/releases/download/v0.109.2/spry-macos.tar.gz"
-      sha256 "963ac1e88047484abfafcc8aadc8a283c648c3fc1cb7cea010031ba17010d86b"
+      sha256 "2d46913afd9bf06113ca530d5368b710519e45d22bdf931be87aa07a778cb9cc"
     end
   end
 
   on_linux do
     url "https://github.com/programmablemd/packages/releases/download/v0.109.2/spry_0.109.2-ubuntu22.04u1_amd64.deb"
-    sha256 "014d074bcf0a33ff8b4ba10d9ab02d3b4d4c734895a4cebb6a058b1f52cbf674"
+    sha256 "0ffa0977a68fec28344c39da95c19ef9fe29bfafbaa8c0beff2e29c1f1edd5ba"
   end
 
   def install
