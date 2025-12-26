89cf7b6b4880d0e5e0091870b8e4384499c06677
Siju Joseph <siju-joseph@netspective.in>
2025-12-26 12:35:20 +0530

    refactor: Move manpage resource definition to macOS block for clarity

--- Formula/spry.rb
+++ Formula/spry.rb
@@ -4,11 +4,6 @@ class Spry < Formula
   version "1.0.0"
   license "MIT"
 
-  resource "manpage" do
-    url "https://github.com/programmablemd/packages/releases/download/v1.0.0/spry_1.0.0-ubuntu22.04u1_amd64.deb"
-    sha256 "b6feda95e000a415f0d3f71f324bba6a2db01a00cc31dfe83d205957ef6b7d98"
-  end
-
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/programmablemd/packages/releases/download/v1.0.0/spry-macos.tar.gz"
@@ -26,17 +21,19 @@ class Spry < Formula
 
   def install
     if OS.mac?
+      resource "manpage" do
+        url "https://github.com/programmablemd/packages/releases/download/v1.0.0/spry_1.0.0-ubuntu22.04u1_amd64.deb"
+        sha256 "b6feda95e000a415f0d3f71f324bba6a2db01a00cc31dfe83d205957ef6b7d98"
+      end
       bin.install "spry-macos" => "spry"
       resource("manpage").stage do
         system "dpkg-deb", "-x", "spry_1.0.0-ubuntu22.04u1_amd64.deb", "."
         man1.install "usr/share/man/man1/spry.1.gz"
       end
     elsif OS.linux?
-      resource("manpage").stage do
-        system "dpkg-deb", "-x", "spry_1.0.0-ubuntu22.04u1_amd64.deb", "."
-        bin.install "usr/bin/spry"
-        man1.install "usr/share/man/man1/spry.1.gz"
-      end
+      system "dpkg-deb", "-x", "spry_1.0.0-ubuntu22.04u1_amd64.deb", "."
+      bin.install "usr/bin/spry"
+      man1.install "usr/share/man/man1/spry.1.gz"
     end
   end
 
